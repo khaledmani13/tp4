e62ef7f120f0c0c1eb8fb7551eb999b89171f9f8
khaled mani <khaled.mani@etu.univ-amu.fr>
2024-10-02 14:21:30 +0200

    qst 2 la liste en compréhension des chifres pairs d'un entier k

--- main.py
+++ main.py
@@ -12,3 +12,6 @@ def suit_fibonacci(n):
     for _ in range (n):
         yield a
         a, b =b, a+b 
+        def est_super_impair(k):
+    chiffres_pairs = [chiffre for chiffre in str(k) if chiffre in '02468']
+    return len(chiffres_pairs) == 0
